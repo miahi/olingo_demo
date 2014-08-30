0b10cc1710d4d626095010e9c29dcb470a7b78df
Mihai Ivascu <me@miahi.ro>
2014-08-30 21:27:08 +0300

    Added CountryReport.

---
+++ src/main/java/edw/olingo/model/CountryReport.java
@@ -0,0 +1,107 @@
+/* Copyright 2011 UNEP (http://www.unep.org)
+ * This file is part of InforMEA Toolkit project.
+ * InforMEA Toolkit is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * InforMEA Toolkit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * You should have received a copy of the GNU General Public License along with
+ * InforMEA Toolkit. If not, see http://www.gnu.org/licenses/.
+ */
+package edw.olingo.model;
+
+import edw.olingo.InvalidValueException;
+import edw.olingo.constants.Treaty;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * Country report primary entity
+ * <br />
+ * @author Cristian Romanescu {@code cristian.romanescu _at_ eaudeweb.ro}
+ * @version 1.4.0, 10/28/2011
+ * @since 1.3.3
+ */
+@Entity
+@Table(name = "informea_country_reports")
+public class CountryReport {
+
+    @Id
+    @Column(name="id")
+    private String id;
+
+    private String treaty;
+    private String country;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date submission;
+    private String url;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated;
+
+    @OneToMany(mappedBy = "country_report", cascade= CascadeType.ALL)
+    private List<CountryReportTitle> titles;
+
+    public Treaty getTreaty() {
+        if(treaty == null || treaty.isEmpty()) {
+            throw new InvalidValueException(String.format("'treaty' property cannot be null (Affected country report with ID:%s)", id));
+        }
+        return Treaty.getTreaty(treaty);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getSubmission() {
+        return submission;
+    }
+
+    public void setSubmission(Date submission) {
+        this.submission = submission;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    public List<CountryReportTitle> getTitles() {
+        return titles;
+    }
+
+    public void setTitles(List<CountryReportTitle> titles) {
+        this.titles = titles;
+    }
+}
---
+++ src/main/java/edw/olingo/model/CountryReportTitle.java
@@ -0,0 +1,71 @@
+/* Copyright 2011 UNEP (http://www.unep.org)
+ * This file is part of InforMEA Toolkit project.
+ * InforMEA Toolkit is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * InforMEA Toolkit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * You should have received a copy of the GNU General Public License along with
+ * InforMEA Toolkit. If not, see http://www.gnu.org/licenses/.
+ */
+package edw.olingo.model;
+
+import javax.persistence.*;
+
+
+/**
+ * Country report title entity
+ * <br />
+ * @author Cristian Romanescu {@code cristian.romanescu _at_ eaudeweb.ro}
+ * @version 1.4.0, 10/28/2011
+ * @since 1.3.3
+ */
+@Entity
+@Table(name="informea_country_reports_title")
+public class CountryReportTitle {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    private CountryReport country_report;
+
+    private String language;
+    private String title;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public CountryReport getCountry_report() {
+        return country_report;
+    }
+
+    public void setCountry_report(CountryReport country_report) {
+        this.country_report = country_report;
+    }
+}
